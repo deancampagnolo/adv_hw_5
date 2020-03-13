2829e24c67b98e4ad331722a04cca26c533b6107
Dean Campagnolo <deancampagnolo@gmail.com>
2020-03-13 00:16:41 -0700

    Auto Sender

--- asg5-oop-opengl/code/main.cpp
+++ asg5-oop-opengl/code/main.cpp
@@ -79,6 +79,7 @@ void scan_options (int argc, char** argv) {
 // Main function.  Iterate over files if given, use cin if not.
 //
 int main (int argc, char** argv) {
+   cout<<"hello world"<<endl;
    sys_info::execname (argv[0]);
    scan_options (argc, argv);
    vector<string> args (&argv[optind], &argv[argc]);
