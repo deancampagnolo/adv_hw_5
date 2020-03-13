232d99e89db6e3f884ec5e702df78fbcf0ec2f87
Dean Campagnolo <deancampagnolo@gmail.com>
2020-03-13 03:58:44 -0700

    Auto Sender

--- asg5-oop-opengl/code/shape.cpp
+++ asg5-oop-opengl/code/shape.cpp
@@ -1,4 +1,5 @@
-// $Id: shape.cpp,v 1.7 2014-05-08 18:32:56-07 - - $
+//dcampagn - Dean Campagnolo
+//aswoiski - Aaron Swoiskin
 #include <cmath>
 #include <typeinfo>
 #include <unordered_map>
@@ -19,7 +20,15 @@ static unordered_map<void*,string> fontname {
     {GLUT_BITMAP_TIMES_ROMAN_24, "Times-Roman-24"},
 };
 
-
+static unordered_map<string,void*> fontcode {
+   {"Fixed-8x13"    , GLUT_BITMAP_8_BY_13       },
+   {"Fixed-9x15"    , GLUT_BITMAP_9_BY_15       },
+   {"Helvetica-10"  , GLUT_BITMAP_HELVETICA_10  },
+   {"Helvetica-12"  , GLUT_BITMAP_HELVETICA_12  },
+   {"Helvetica-18"  , GLUT_BITMAP_HELVETICA_18  },
+   {"Times-Roman-10", GLUT_BITMAP_TIMES_ROMAN_10},
+   {"Times-Roman-24", GLUT_BITMAP_TIMES_ROMAN_24},
+};
 
 ostream& operator<< (ostream& out, const vertex& where) {
     out << "(" << where.xpos << "," << where.ypos << ")";
@@ -30,11 +39,9 @@ shape::shape() {
     DEBUGF ('c', this);
 }
 
-text::text (void* glut_bitmap_font, const string& textdata):
-glut_bitmap_font(glut_bitmap_font), textdata(textdata) {
+text::text (void* glut_bitmap_font_, const string& textdata_):
+      glut_bitmap_font(glut_bitmap_font_), textdata(textdata_) {
     DEBUGF ('c', this);
-    //this->glut_bitmap_font = glut_bitmap_font;
-    //this->textdata = textdata;
 }
 
 ellipse::ellipse (GLfloat width, GLfloat height):
@@ -46,62 +53,43 @@ circle::circle (GLfloat diameter): ellipse (diameter, diameter) {
     DEBUGF ('c', this);
 }
 
-polygon::polygon (const vertex_list& vertices): vertices(vertices) {
+polygon::polygon (const vertex_list& vertices_): vertices(vertices_) {
     DEBUGF ('c', this);
-
 }
-triangle::triangle (const vertex_list& v) : polygon(v){
+
+triangle::triangle (const vertex_list& verticies_) : polygon(verticies_){
     DEBUGF ('c', this);
 }
 
 equilateral::equilateral (GLfloat width): 
-    triangle ({{-width/2, -((sqrt(3)/2)*width)/2}, 
-                {width/2, -((sqrt(3)/2)*width)/2}, 
-                {0, ((sqrt(3)/2)*width)/2}}) {
+triangle ({{-width/2, -((sqrt(3)/2)*width)/2}, 
+            {width/2, -((sqrt(3)/2)*width)/2}, 
+            {0, ((sqrt(3)/2)*width)/2}}) {
     DEBUGF ('c', this);
-
 }
+
 rectangle::rectangle (GLfloat width, GLfloat height):
-polygon({{0.0, 0.0}, {width, 0.0}, {width, height}, {0.0, height}}) {
-    
-    //figure out how to make a rectangle;
+polygon({{-width/2, -height/2}, {width/2, -height/2}, 
+        {width/2, height/2}, {-width/2, height/2}}) {
     DEBUGF ('c', this << "(" << width << "," << height << ")");
 }
 
 diamond::diamond (GLfloat width, GLfloat height):
-polygon({{0.0, 0.0}, {width/2, -height/2}, {width, 0.0}, {width/2, height/2}}) {
-    
-    //figure out how to make a rectangle;
+polygon({{0.0, -height/2}, {width/2, 0.0}, {0.0, height/2}, {-width/2, 0.0}}) {
     DEBUGF ('c', this << "(" << width << "," << height << ")");
 }
 
 square::square (GLfloat width): rectangle (width, width) {
     DEBUGF ('c', this);
-
 }
 
 void text::draw (const vertex& center, const rgbcolor& color, bool b) const {
-    
     DEBUGF ('d', this << "(" << center << "," << color << ")");
-    if(b == true)
-       for(int w = 0 ; w< 5; w++) break;
-   //  char fakeParam[] = "fake";
-  //char *fakeargv[] = { fakeParam, NULL };
-  //int fakeargc = 1;
-  //glutInit( &fakeargc, fakeargv ); 
-  // rgbcolor color;
-      //glPushMatrix();
-     //auto butt = this->textdata;
-      glColor3ubv(color.ubvec);
-      glRasterPos2f (center.xpos, center.ypos);
-      //glDisable(GL_TEXTURE);
-   // glDisable(GL_TEXTURE_2D);
-      
-     // glutBitmapString(glut_bitmap_font, textdata);
-     for (auto ch: textdata) glutBitmapCharacter (glut_bitmap_font, ch);
-     glutSwapBuffers();
-    // glEnable(GL_TEXTURE);
-   // glEnable(GL_TEXTURE_2D);
+    
+    glColor3ubv(color.ubvec);
+    glRasterPos2f (center.xpos, center.ypos);
+    for (auto ch: textdata) glutBitmapCharacter (glut_bitmap_font, ch);
+    glutSwapBuffers();
 }
 
 void ellipse::draw (const vertex& center, const rgbcolor& color, bool b) const {
